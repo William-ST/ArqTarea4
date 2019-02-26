54b967f573694465fbfdf862c2e81277e69429b9
william_ST <wsulcata@everis.com>
2019-02-26 02:45:33 -0500

    first commit

---
+++ app/src/main/java/com/example/calculadora/MathOperation.java
@@ -0,0 +1,11 @@
+package com.example.calculadora;
+
+/**
+ * Created by William_ST on 26/02/19.
+ */
+
+public class MathOperation {
+    public double addition(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+}
